12ac7a61bd98afcea5cf7634821ba853f009a05b
qlls <phuc88.klvn@gmail.com>
2025-07-30 14:45:25 +0700

    Update PrivateRoute.js

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -3,18 +3,18 @@ import { Navigate, useLocation } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext'; // Sử dụng hook useAuth
 
 const PrivateRoute = ({ children, allowedRoles }) => {
-  const { auth } = useAuth();
+  const { isLoggedIn, role } = useAuth();
   const location = useLocation();
 
   // 1. Kiểm tra xem người dùng đã đăng nhập chưa
-  if (!auth.isLoggedIn) {
+  if (!isLoggedIn) {
     // Nếu chưa, chuyển hướng về trang đăng nhập
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
   // 2. Kiểm tra xem vai trò của người dùng có được phép truy cập không
   // So sánh vai trò của người dùng (auth.role) với danh sách vai trò được phép (allowedRoles)
-  if (!allowedRoles.includes(auth.role)) {
+  if (!allowedRoles.includes(role)) {
     // Nếu không có quyền, chuyển hướng đến trang "Unauthorized"
     return <Navigate to="/unauthorized" state={{ from: location }} replace />;
   }
