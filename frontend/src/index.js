db1766d45166de35e25ac9136cf7063d133936ce
qlls <phuc88.klvn@gmail.com>
2025-07-30 22:41:46 +0700

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,17 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <BrowserRouter basename="/nvp/">
+    <BrowserRouter basename="/nvp">
+      <AuthProvider>
         <App />
-      </BrowserRouter>
-    </AuthProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
