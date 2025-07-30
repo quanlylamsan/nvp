b7472ce585585d9df97de3c7369d1e923cc1464f
qlls <phuc88.klvn@gmail.com>
2025-07-30 22:38:47 +0700

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <BrowserRouter basename="/nvp/frontend">
+      <BrowserRouter basename="/nvp/">
         <App />
       </BrowserRouter>
     </AuthProvider>
